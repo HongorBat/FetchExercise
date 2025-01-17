2333edc4ed16c047481034afc61450a614fbe131
Hongor <hongor0407@gmail.com>
2025-01-17 15:27:59 -0500

    update viewmodel to make network request to retrieve list items

--- app/src/main/java/com/example/fetchcodingexercise/ui/screens/ExerciseViewModel.kt
+++ app/src/main/java/com/example/fetchcodingexercise/ui/screens/ExerciseViewModel.kt
@@ -3,19 +3,29 @@ package com.example.fetchcodingexercise.ui.screens
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.fetchcodingexercise.data.FetchRepository
+import com.example.fetchcodingexercise.network.ListItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.io.IOException
+import javax.inject.Inject
 
 // Different states of the ui depending on the network call success
 sealed interface AppUiState{
     object Loading : AppUiState
-    data class Success(val response : String) : AppUiState
+    data class Success(val response : List<ListItem>) : AppUiState
     object Error : AppUiState
 }
 
 @HiltViewModel
-class ExerciseViewModel {
-    // Initial ui state is always loading with private set
-    var uiState by mutableStateOf(AppUiState.Loading)
+class ExerciseViewModel @Inject constructor(
+    private val repository: FetchRepository
+) : ViewModel() {
+    // mutable state that hold the status of most recent network request
+    // initial value is always loading.
+    var uiState : AppUiState by mutableStateOf(AppUiState.Loading)
         private set
 
     // call getListItems() on init so we can display status
@@ -25,6 +35,12 @@ class ExerciseViewModel {
 
     // get list items from the api
     private fun getListItems() {
-        TODO("Not yet implemented")
+        viewModelScope.launch {
+            uiState = try {
+                AppUiState.Success(repository.getListItems())
+            } catch (e : IOException){
+                AppUiState.Error
+            }
+        }
     }
 }
