19a7beee6d541d5512a941bcaf02a1e9cd44465f
Hongor <hongor0407@gmail.com>
2025-01-17 15:05:26 -0500

    show homescreen from the app composable with top bar

--- app/src/main/java/com/example/fetchcodingexercise/FetchCodingExerciseApp.kt
+++ app/src/main/java/com/example/fetchcodingexercise/FetchCodingExerciseApp.kt
@@ -1,8 +1,57 @@
 package com.example.fetchcodingexercise
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.fetchcodingexercise.ui.screens.HomeScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FetchCodingExerciseApp(){
+    // When content pulled up top bar collapses and when content pulled down top bar reappears
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { TopBar(scrollBehavior = scrollBehavior) }
+    ) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            HomeScreen(contentPaddingValues = it)
+        }
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier){
+    //Use TopBar to display title
+    //Might use it to change between lists.
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineMedium
+            )
+        },
+        modifier = modifier
+    )
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun TopBarPreview(){
+    FetchCodingExerciseApp()
 }
